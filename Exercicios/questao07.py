7470c74df691d1973a7c22ef45151049b8af2e0f
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2019-11-11 15:26:07 -0300

    Delete questao07.py

--- Exercicios/questao07.py
+++
@@ -1,4 +0,0 @@
-print(f'Exercício 07 - Conversão de TEMPERATURA (Celsius para Fahrenheit)')
-c = int(input(f'Informe a TEMPERATURA em graus Celsius: '))
-f = 9*c/5+32
-print(f'Temperatura em FAHRENHEIT: {f}')
