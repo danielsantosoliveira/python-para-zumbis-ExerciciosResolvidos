e36c03d0a054f46f0baded3a2ba5e6ed966ae620
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2019-11-11 15:25:39 -0300

    Delete questao04.py

--- Exercicios/questao04.py
+++
@@ -1,6 +0,0 @@
-print(f'Exercício 4 - Aumento de Salário')
-salario = float(input(f'Informe o valor do SALÁRIO: '))
-porcentagem = float(input(f'Informe a PORCETAGEM do aumento: '))
-aumento = salario * (porcentagem/100)
-novoSalario = salario + aumento
-print(f' Valor do AUMENTO: R$ {aumento} n\ Valor do NOVO Salário: R$ {novoSalario}')
