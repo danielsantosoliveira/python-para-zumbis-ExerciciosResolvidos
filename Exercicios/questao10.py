75ec71fce869602f7289fab3545d34a2db0a203f
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2019-11-11 15:26:44 -0300

    Delete questao10.py

--- Exercicios/questao10.py
+++
@@ -1,7 +0,0 @@
-print('Exercício 10 - Tempo de Vida')
-qtdCgoDia = int(input('Informe a QUANTIDADE de cigarro(s) fumado(s) por dia: '))
-qtdAnsFmu = int(input('Informe a QUANTIDADE de anos que já fumou: '))
-ttlPerMin = (qtdCgoDia * (qtdAnsFmu*365)) * 10
-totalDias = ttlPerMin / 1440 
-print(f'Total em MINUTOS perdido: {ttlPerMin} min')
-print('Total em DIAS perdido: {:.2} dias'.format(totalDias))
