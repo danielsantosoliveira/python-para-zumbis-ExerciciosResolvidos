e491596742fd693547cb9c6493e8c5caa1dbda94
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2019-11-11 15:25:05 -0300

    Delete questao01.py

--- Exercicios/questao01.py
+++
@@ -1,6 +0,0 @@
-print(f'Exercício 1 - Soma de dois números')
-n1 = int(input(f'Informe o PRIMEIRO número: '))
-n2 = int(input(f'Informe o SEGUNDO número: '))
-soma = n1 + n2
-print(f'Total da soma dos dois números: {soma}')
-
