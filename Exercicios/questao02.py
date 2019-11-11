d06ebf2df5bc4aebb02c69f1c79dbdf88924d83e
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2019-11-11 15:25:16 -0300

    Delete questao02.py

--- Exercicios/questao02.py
+++
@@ -1,4 +0,0 @@
-print(f'Exercício 2 - Conversão METROS para MILÍMETROS')
-n = float(input(f'Informe qualquer valor em METROS para conversão: '))
-conversao = n * 1000
-print(f'Valor convertido em MILÍMETROS: {conversao}mm')
