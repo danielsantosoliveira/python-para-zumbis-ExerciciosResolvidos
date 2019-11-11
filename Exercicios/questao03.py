eb5c5188a7010349823cea33b9b64ff1bf638a8c
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2019-11-11 15:25:27 -0300

    Delete questao03.py

--- Exercicios/questao03.py
+++
@@ -1,7 +0,0 @@
-print(f'Exercício 3 - Cálculo em Segundos')
-dias = int(input(f'Informe a quantidade de DIAS: '))
-horas = int(input(f'Informe a quantidade de HORAS: '))
-minutos = int(input(f'Informe a quantidade de MINUTOS: '))
-segundos = int(input(f'Informe a quantidade de SEGUNDOS: '))
-calculo = ((dias * 86400) + (horas * 3600) + (minutos * 60)) + segundos
-print(f'Total em segundos: {calculo}s')
